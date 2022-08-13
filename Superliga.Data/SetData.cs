00695bb4d8705a7ea01406791d3c0f0e371af341
Lara_Anabel_Garcia <glaraanabelperez@gmail.com>
2022-08-13 20:05:21 -0300

    agregando proyecto front, leyengo archivo excell y creando tabla para guardarlo en primera instancia

--- Superliga.Data/SetData.cs
+++ Superliga.Data/SetData.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -14,6 +16,50 @@ namespace Superliga.Data
         public SetData()
         {
              partner= new Partner();
+            ReadCvs();
+
+        }
+
+        public void ReadCvs()
+        {
+            //List<string> lineas = new List<string>();
+            //using (StreamReader sr = new StreamReader(Path))
+            //{
+            //    sr.ReadLine();
+            //    while (sr.Peek() != -1)
+            //    {
+            //        lineas.Add(sr.ReadLine());
+            //    }
+            //}
+
+            var dtExcelTable = new DataTable();
+            dtExcelTable.Rows.Clear();
+            dtExcelTable.Columns.Clear();
+            dtExcelTable.Columns.Add("Nombre");
+            dtExcelTable.Columns.Add("Edad");
+
+            var reader = new StreamReader(File.OpenRead(@"C:\Users\Lara\source\repos\Superliga.Net.Angular\Superliga\DataCvs\socios.csv"));
+            List<string> listA = new List<string>();
+            List<string> listB = new List<string>();
+
+            var d = 0;
+            while (!reader.EndOfStream)
+            {
+                var line = reader.ReadLine();
+                var values = line.Split(';');
+
+                var dr = dtExcelTable.NewRow();
+                dr[d] = values[0];
+                d = d + 1;
+
+                listA.Add(values[0]);
+                listB.Add(values[1]);
+
+                dtExcelTable.Rows.Add(dr);
+
+
+                
+            }
         }
     }
 }
